e07e48c476c4b9c93a6f204c5c59e35037335ff0
Matthew Giem <matthew.giem@gmail.com>
2017-10-11 12:27:16 -0700

    need to fix glitch in move options

--- dundgen_game.py
+++ dundgen_game.py
@@ -10,7 +10,6 @@ import os
 #move player unless invalid movement
 #check for win or loss
 #clear the screen and redraw the grid
-
 CELLS = [
         (0,0), (1,0), (2,0), (3,0), (4,0),
         (0,1), (1,1), (2,1), (3,1), (4,1),
@@ -21,6 +20,8 @@ CELLS = [
 
 def clear_screen():
     os.system('cls' if os.name == 'nt' else 'clear')
+
+clear_screen()
 
 def get_locations():
     return random.sample(CELLS, 3)
@@ -44,37 +45,48 @@ def get_moves(player):
     x = player[0]
     y = player[1]
     if y == 0:
-        moves.pop()
+        moves.remove("UP")
     if y == 4:
-        moves.pop(2)
+        moves.remove("DOWN")
     if x == 0:
-        moves.pop(1)
+        moves.remove("LEFT")
     if x == 4:
-        moves.pop(0)
+        moves.remove("RIGHT")
     return moves
 
 dragon, player, door = get_locations()
+wrong_move = 0
 
 while True:
     print("Welcome to the Dundgen")
     print("You're currently in room {}".format(player))  #fill with player room
     print("You can move {}".format(", ".join(get_moves(player))))  #fill with available moves
     print("You can QUIT to quit")
-
+    if wrong_move == 1:
+        print("you cant move there!")
+    if wrong_move == 2:
+        print("that move isn't an option")
     move = raw_input("> ")
     move = move.upper()
 
     if move == "QUIT":
         break
-
-    player = move_player(player, move)
-    if player == door:
-        print("congradulations you fond the door and won!!")
-        break
-    if player == dragon:
-        print("I'm sorry you ran in to the dragon!! you lost")
-        break
-    clear_screen()
+    if move in get_moves(player):
+        wrong_move = 0
+        player = move_player(player, move)
+        if player == door:
+            print("congradulations you fond the door and won!!")
+            break
+        if player == dragon:
+            print("I'm sorry you ran in to the dragon!! you lost")
+            break
+        clear_screen()
+    elif move in ["RIGHT", "LEFT", "DOWN", "UP"]:
+        clear_screen()
+        wrong_move = 1
+    else:
+        clear_screen()
+        wrong_move = 2
 
     #good move => change player position
     #bad move => alert player and don't change position
